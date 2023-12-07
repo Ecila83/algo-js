d562917c097d88093dfa00baae5e80003f6f8936
Alice Marique <mariquealice83@gmail.com>
2023-12-07 16:49:16 +0100

    2.2

---
+++ 2.2.js
@@ -0,0 +1,10 @@
+let $min = prompt("votre nombre minimum ? :");
+let $max = prompt("votre nombre maximum ? :");
+let $current = prompt("nombre aleatoire? :");
+
+if ($current < $max && $current > $min){
+    console.log(`${$current} se situe entre les deux autres`);
+}else{
+    console.log($current + " ne se situe pas entre les deux autres.");
+}
+
