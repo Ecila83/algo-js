4a1e7411cb9393ee44a0bd34f83405177cba59db
Alice Marique <mariquealice83@gmail.com>
2023-12-08 11:33:51 +0100

    2.7

---
+++ 2.7.js
@@ -0,0 +1,11 @@
+let n = parseFloat(prompt("choisissez un nombres: "));
+let result = 0;
+let number = 0;
+
+for (i = 0; i<n; i++){
+    number = parseFloat(prompt('un nombre'));
+     result += number;
+    
+}
+console.log(result);
+
