6ddab041409c184ee5d49eec1892eb0d5aeead03
Alice Marique <mariquealice83@gmail.com>
2023-12-08 09:59:09 +0100

    2.4

---
+++ 2.4.js
@@ -0,0 +1,23 @@
+// for(i=1;i<=100;i++){
+// if(i%2==0){
+//         console.log(i/2);
+//     }
+//     else{
+//         console.log(i*3);
+//     }
+
+// }
+
+
+
+let i = 1;
+while (i<=100){
+    if(i%2==0){
+        console.log(i/2);
+    }
+    else{
+        console.log(i*3);
+    }
+    
+    i++;
+}
