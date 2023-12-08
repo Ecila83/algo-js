042fa1cf2587251889a1efd86d7c55249bd04970
Alice Marique <mariquealice83@gmail.com>
2023-12-08 10:15:15 +0100

    2.5

---
+++ 2.5.js
@@ -0,0 +1,7 @@
+
+let nbr = prompt("veuillez entrer votre nombre preféré : ");
+while (nbr != 42){
+    console.log("êtes vous sur?" );
+    nbr = prompt("veuillez entrer votre nombre preféré : ");
+}
+alert("Votre nombre preféré est 42");
